77f9f88f18549897ec9d952f0da8a4a8c1a99c44
Tomoaki-git <77450533+Tomoaki-git@users.noreply.github.com>
2021-02-20 11:04:44 +0900

    ログイン機能、ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password,              confirmation: true
+  validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
   validates :password_confirmation, presence: true
   validates :nickname,              presence: true
-  validates :family_name,           presence: true
-  validates :first_name,            presence: true
-  validates :family_name_kana,      presence: true
-  validates :first_name_kana,       presence: true
+  validates :family_name,           presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_kana,      presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana,       presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday,              presence: true
 
 end
