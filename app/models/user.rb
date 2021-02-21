10d78fa5dce38eb9d751f264ae1540f3e924c148
Tomoaki-git <77450533+Tomoaki-git@users.noreply.github.com>
2021-02-21 15:12:52 +0900

    ユーザー管理機能：修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,27 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
-  validates :password_confirmation, presence: true
-  validates :nickname,              presence: true
-  validates :family_name,           presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :family_name_kana,      presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana,       presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :birthday,              presence: true
+  with_options presence: true do
+
+    with_options format:{ with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/ } do
+      validates :password
+      validates :password_confirmation
+    end
+
+    validates :nickname
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/} do
+      validates :family_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/} do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+
+    validates :birthday
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,9 +11,10 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
     end
-    context '登録できない場合' do
+
+    context '登録できない場合(:email)' do
       it 'メールアドレスがないと登録できない' do
-        @user.email = " "
+        @user.email = nil
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
@@ -24,12 +25,15 @@ RSpec.describe User, type: :model do
         expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
       it 'メールアドレスは＠を含まないと登録できない' do
-        @user.password = "sample"
+        @user.email = "sample"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include("Email is invalid")
       end
+    end
+
+    context '登録できない場合(:password)' do
       it 'パスワードがないと登録できない' do
-        @user.password = ""
+        @user.password = nil
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
@@ -38,17 +42,89 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
+      it '６文字以上でないと登録できない' do
+        @user.password = "aa111"
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it 'パスワードが英語のみでは登録できない' do
+        @user.password = "aaaaaa"
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it 'パスワードが数字のみでは登録できない' do
+        @user.password = "111111"
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it 'パスワードが全角では登録できないこと' do
+        @user.password = "ａａａａａａ"
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+    end
+
+    context '登録できない場合(:family_name)' do
+      it '「family_name」がないと登録できない' do
+        @user.family_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name can't be blank")
+      end
       it '「お名前（全角）」がひらがな・カタカナ・漢字以外の場合は登録できない' do
         @user.family_name = "aaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name is invalid")
+      end
+    end
+
+    context '登録できない場合(:first_name)' do
+      it '「first_name」がないと登録できない' do
+        @user.first_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it '「お名前（全角）」がひらがな・カタカナ・漢字以外の場合は登録できない' do
         @user.first_name = "aaaa"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name is invalid", "First name is invalid")
+        expect(@user.errors.full_messages).to include("First name is invalid")
+      end
+    end
+
+    context '登録できない場合(:family_name_kana)' do
+      it '「family_name_kan」がないと登録できない' do
+        @user.family_name_kana = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana can't be blank")
       end
       it '「お名前カナ（全角）」がカタカナ意外の場合は登録できない' do
         @user.family_name_kana = "imei.name.last"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana is invalid")
+      end
+    end
+
+    context '登録できない場合(:first_name_kana)' do
+      it '「first_name_kana」がないと登録できない' do
+        @user.first_name_kana = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+      it '「お名前カナ（全角）」がカタカナ意外の場合は登録できない' do
         @user.first_name_kana = "Gimei.name.first"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name kana is invalid", "First name kana is invalid")
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
+      end
+    end
+
+    context '登録できない場合(:birthday)' do
+      it '「birthday」がないと登録できない' do
+        @user.birthday = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
     end
   end
