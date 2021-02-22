b93b886f415d1ac298803e5c52685260129a0494
Tomoaki-git <77450533+Tomoaki-git@users.noreply.github.com>
2021-02-22 13:46:54 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :move_to_session, only: [:new]
   def index
     
+  end
+
+  def new
+      @item = Item.new
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def move_to_session
+    unless user_signed_in?
+      redirect_to  user_session_path
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
