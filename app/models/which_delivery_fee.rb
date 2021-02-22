18c4643e481e8df00fb7f0f140bc06f6109e2265
Tomoaki-git <77450533+Tomoaki-git@users.noreply.github.com>
2021-02-22 19:46:37 +0900

    itemテーブルのバリデーション修正

--- app/models/which_delivery_fee.rb
+++ app/models/which_delivery_fee.rb
@@ -2,7 +2,7 @@ class WhichDeliveryFee < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
     { id: 1, name: '着払い(購入者負担)' },
-    { id: 2, name: '未使用に近い' }
+    { id: 2, name: '送料込み(出品者負担)' }
   ]
 
   include ActiveHash::Associations
